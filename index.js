f9f1a47a4ebace12a9eb05b9b7dde60b28717acd
addkolon <mattias@addkolon.se>
2022-03-07 11:37:28 +0100

    Some fixes and some fuck ups

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 const canvas = document.querySelector(".canvas1");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 500;
+canvas.width = 1200;
+canvas.height = 800;
 
 const gameSpeedEl = document.querySelector("h2");
 
 let frame = 0;
-let gameSpeed = 1;
+let gameSpeed = 5;
 
 const boat = {
   x: 100,
@@ -20,6 +20,52 @@ const boat = {
   speed: 5,
   moving: false,
 };
+
+
+// BACKGROUND - Mattias
+const bg1 = new Image();
+bg1.src = "./sprite/bg1.png"
+const bg2 = new Image();
+bg2.src = "./sprite/bg2.png"
+const bg3 = new Image();
+bg3.src = "./sprite/bg3.png"
+const bg4 = new Image();
+bg4.src = "./sprite/bg4.png"
+
+class Layer {
+  constructor(image, speedModifier) {
+    this.x = 0;
+    this.y = 0;
+    this.width = 2400;
+    this.height = 125;
+    this.x2 = this.width;
+    this.image = image;
+    this.speedModifier = speedModifier;
+    this.speed = 10 * this.speedModifier;
+  }
+  update() {
+    this.speed = 10 * this.speedModifier;
+    if (this.x <= -this.width) {
+      this.x = this.width + this.x2 - this.speed;
+    }
+    if (this.x2 <= -this.width) {
+      this.x2 = this.width + this.x - this.speed;
+    }
+    this.x = this.x - this.speed;
+    this.x2 = this.x2 - this.speed;
+  }
+  draw() {
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.image, this.x2, this.y2, this.width, this.height);
+  }
+}
+
+const layer1 = new Layer(bg4, 0.04);
+const layer2 = new Layer(bg3, 0.06);
+const layer3 = new Layer(bg2, 0.08);
+const layer4 = new Layer(bg1, 0.10);
+
+const gameBg = [layer1, layer2, layer3, layer4];
 
 const boatSprite = new Image();
 boatSprite.src = "./sprite/boat2.png";
@@ -72,6 +118,9 @@ window.addEventListener("keyup", (e) => {
   delete keys[e.key];
 });
 
+// let x = 0;
+// let x2 = 2400;
+
 let fps, fpsInterval, startTime, now, then, elapsed;
 
 const startAnimating = (fps) => {
@@ -88,6 +137,10 @@ const animate = () => {
   if (elapsed > fpsInterval) {
     then = now - (elapsed % fpsInterval);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    gameBg.forEach(object => {
+      object.update();
+      object.draw();
+    });
     drawSprite(
       boatSprite,
       boat.width * boat.frameX,
